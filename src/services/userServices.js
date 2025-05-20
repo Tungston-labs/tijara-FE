155304d7625528764aef3a1f3bf128c6f14d2525
Dilshima-p <dilshima@tungstonlabs.com>
2025-05-20 15:45:40 +0530

    Get product and pagination are set up

--- src/services/userServices.js
+++ src/services/userServices.js
@@ -21,11 +21,10 @@ export const fetchAgents = async () => {
     return response
 }
 
-export const fetchItems = async () => {
+export const fetchItems = async ({page}) => {
     console.log("hello")
-    const response = API.get(`product/get-products`)
-    const data = await response.json();
-     return { data: data.items, totalPages: data.totalPages };
+    const response = API.get(`product/get-products?page=${page}`)
+    return response
 }
 
 export const fetchSubItems = async () => {
