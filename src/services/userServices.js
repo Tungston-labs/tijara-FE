38ba82553790898f3ed596bf7117b3d940ba8e9b
Ajay-tungston <you@example.com>
2025-06-03 11:35:45 +0530

    registration screen is in progrees

--- src/services/userServices.js
+++ src/services/userServices.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 const API = axios.create({
   withCredentials: true,
-  baseURL: "http://localhost:5000",
+  baseURL: "http://178.248.112.16:8080",
   headers: {
     "Content-Type": "application/json",
     Authorization: `Bearer ${localStorage.getItem("accessToken")}`,
